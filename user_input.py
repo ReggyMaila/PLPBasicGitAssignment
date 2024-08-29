03f4f2990a46a0119056662af4a02dea088701fc
ReggyMaila <reginald.mokagane@gmail.com>
2024-08-29 17:55:06 +0200

    Python Commit

---
+++ user_input.py
@@ -0,0 +1,10 @@
+#Declaring variable with user input
+
+
+name = input("Enter Your Name: ")#
+age = input("Enter Your Age: ")
+location = input("Enter your Location: ")
+
+#printing output
+
+print(f'Hello {name} you are {age} years old and lives in {location}')
